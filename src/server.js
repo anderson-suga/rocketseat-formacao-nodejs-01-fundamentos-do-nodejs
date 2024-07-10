1c45765160ec1af2dad997480ec0140087ff8bdf
Anderson Suga <anderson.suga@gmail.com>
2024-07-10 18:02:11 -0300

    Capturando query parameters

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
 import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
 // - Criar usuários
 // - Listagem usuários
@@ -53,6 +54,11 @@ const server = http.createServer(async (req, res) => {
 
     if (route) {
         const routeParams = req.url.match(route.path);
+
+        const { query, ...params } = routeParams.groups;
+
+        req.params = params;
+        req.query = query ? extractQueryParams(query) : {};
 
         req.params = { ...routeParams.groups };
 
